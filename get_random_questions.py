fecb6dba1f3b2cc08aed324f6d309d4a0769d338
ilmir <ilmir531@gmail.com>
2022-11-02 15:06:48 +0300

    отформатировал код по стандарту PEP
    -отформатировал код
    -удалил ненужные файлы

--- get_random_questions.py
+++ get_random_questions.py
@@ -1,10 +1,10 @@
-import redis
 import json
 import random
+
 
 def get_questions(connect_to_redis):
     get_questions = connect_to_redis.get('questions')
     questions = json.loads(get_questions)
     question_number = random.choice(list(questions))
     question = questions.get(question_number)['question']
-    return question,question_number
+    return question, question_number
--- json_parse.py
+++ json_parse.py
@@ -1,9 +1,10 @@
 # import os
-import redis
 import json
 
+import redis
 
-def parse_json(user_info,chat_id, question_number,messanger_name):
+
+def parse_json(user_info, chat_id, question_number, messanger_name):
     user_info = json.loads(user_info)
     user_info[f"user_{messanger_name}_{chat_id}"] = {"last_asked_question": question_number}
     user_info_json = json.dumps(user_info)
