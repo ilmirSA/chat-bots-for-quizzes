5185e3a2bf893e79140cfa40358174318d1467f1
ilmir <ilmir531@gmail.com>
2022-11-06 11:29:04 +0300

    удалил файл collect_data

--- collect_data.py
+++
@@ -1,4 +0,0 @@
-def collect_data(chat_id, question_number, messanger_name):
-    user_name_key = f"user_{messanger_name}_{chat_id}"
-    user_value = {"last_asked_question": question_number}
-    return user_name_key, user_value
